4692e91306d8247c1185b8f4cb711844b7bbed1c
hafij <hafij.alasad23@gmail.com>
2025-02-25 00:47:21 +0600

    feat: data streaming implemented

--- stream-buffer.js
+++ stream-buffer.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // creating a server with raw node.js
 
@@ -8,7 +9,20 @@ const server = http.createServer();
 
 server.on('request', (req, res) =>{
     console.log(req.url, req.method);
-    res.end("Hello world!")
+    if(req.url === "/read-file" && req.method === "GET"){
+        const readableStream = fs.createReadStream(process.cwd() + '/texts/long-read.txt');
+
+        readableStream.on('data', (buffer) => {
+            res.write(buffer)
+        })
+        readableStream.on('end', ()=>{
+            res.end("Streaming ended.")
+        })
+    
+    }else{
+        res.end("Hello world!")
+    }
+
 })
 
 server.listen(5000, () =>{
